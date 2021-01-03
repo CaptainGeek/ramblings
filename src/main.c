e5bf60a46bb97395670938dfe8b6050b906dd76f
CaptainGeek <CaptainGeek@users.noreply.github.com>
2021-01-04 00:46:27 +0100

    Changes to be committed: 	modified:   README.md 		Small formal changes 	modified:   src/main.c 		Added file comment at the begging of the .C source file.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (C) 2021 Captain Geek
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSE.md for more information.
+ */
+
+
 #include <stdio.c>
 
 int main(int argc, char* argv)
